4553a8f5331b397871aafeb680de38ef2d82c341
JoseP95 <jm_98_95@hotmail.com>
2015-10-31 16:07:11 +0100

    Cambio de referencias

--- Classes/Mechanics.cpp
+++ Classes/Mechanics.cpp
@@ -1,14 +1,14 @@
-#include "HelloWorldScene.h"
+#include "MechanicsScene.h"
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
+Scene* Mechanics::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
     
     // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
+    auto layer = Mechanics::create();
 
     // add layer as a child to scene
     scene->addChild(layer);
@@ -22,7 +22,7 @@ Scene* HelloWorld::createScene()
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
+bool Mechanics::init()
 {
     //////////////////////////////
     // 1. super init first
@@ -42,7 +42,7 @@ bool HelloWorld::init()
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+                                           CC_CALLBACK_1(Mechanics::menuCloseCallback, this));
     
 	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
@@ -67,8 +67,8 @@ bool HelloWorld::init()
     // add the label as a child to this layer
     this->addChild(label, 1);
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    // add "Mechanics" splash screen"
+    auto sprite = Sprite::create("Mechanics.png");
 
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
@@ -80,7 +80,7 @@ bool HelloWorld::init()
 }
 
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
+void Mechanics::menuCloseCallback(Ref* pSender)
 {
     Director::getInstance()->end();
 
